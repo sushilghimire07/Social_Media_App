101359241cfb6267ab8cee88323abd06c59e47b0
Your New Name <sushilghimire1010@gmail.com>
2025-11-10 13:17:18 +0545

    bla

---
+++ server/pages/api/inngest.js
@@ -0,0 +1,4 @@
+// pages/api/inngest.js
+import { inngest } from "../../inngest/index.js"; // path to your Inngest functions file
+
+export default inngest.handler();
--- server/pages/inngest.js
+++
@@ -1,4 +0,0 @@
-// pages/api/inngest.js
-import { inngest } from "../inngest/index.js"; // path to your Inngest functions file
-
-export default inngest.handler();
