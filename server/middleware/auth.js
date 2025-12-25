078d1256c300864307c7b44ef97922018862c3df
Your New Name <sushilghimire1010@gmail.com>
2025-12-25 18:19:11 +0545

    Refactor auth middleware; improve code formatting for better readability

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,17 +1,20 @@
 // middleware/auth.js
 export const protect = async (req, res, next) => {
   try {
-    const authData = await req.auth();   // call once
+    const authData = await req.auth(); // call once
 
     if (!authData?.userId) {
-      return res.status(401).json({ success: false, message: "not authenticated" });
+      return res
+        .status(401)
+        .json({ success: false, message: "not authenticated" });
     }
 
     req.userId = authData.userId;
     next();
-
   } catch (err) {
     console.log("Auth error:", err);
-    return res.status(401).json({ success: false, message: "Authentication failed" });
+    return res
+      .status(401)
+      .json({ success: false, message: "Authentication failed" });
   }
 };
